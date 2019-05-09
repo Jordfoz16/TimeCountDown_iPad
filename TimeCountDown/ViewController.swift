d09c207103fd3c66ec57f7f2e06256bea8d7c124
Jordfoz16 <jordfoz@gmail.com>
2019-05-09 09:58:21 +0100

    Initial Commit

---
+++ TimeCountDown/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeCountDown
+//
+//  Created by Jordan Foster on 09/05/2019.
+//  Copyright © 2019 Silience. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
