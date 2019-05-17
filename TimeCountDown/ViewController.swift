d35dc17aca466b107d87b54cff21335a444ddf22
Jordfoz16 <jordfoz@gmail.com>
2019-05-17 12:36:28 +0100

    Timer Update

--- TimeCountDown/ViewController.swift
+++ TimeCountDown/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var randomWord: UILabel!
+    @IBOutlet weak var secondsLabel: UILabel!
+    
+    var wordTime = 45.0
     
     let wordArray = ["Nature", "Secret", "Imagination", "Old","Water","Abstract","Weathered","Calm","Fashion","Colourful"]
     
@@ -21,12 +24,26 @@ class ViewController: UIViewController {
 
     @IBAction func getRandomWord(_ sender: Any) {
         
+        var countDownTime = wordTime
+        let stepDec = 0.01
+        
         let randomNum = Int.random(in: 0 ..< wordArray.count)
         
         randomWord.text =  wordArray[randomNum]
         
-        let timer = Timer.scheduledTimer(withTimeInterval: 45.0, repeats: false) { timer in
+        let timer = Timer.scheduledTimer(withTimeInterval: countDownTime, repeats: false) { timer in
             self.randomWord.text = "Press the button to get word"
+        }
+        
+        let timer2 = Timer.scheduledTimer(withTimeInterval: stepDec, repeats: true) { timer2 in
+            countDownTime = countDownTime - stepDec
+            
+            self.secondsLabel.text = String(format:"%.2f", countDownTime)
+            
+            if(countDownTime <= 0.0){
+                timer2.invalidate()
+                self.secondsLabel.text = String(format:"%.2f", 0.0)
+            }
         }
     }
     
